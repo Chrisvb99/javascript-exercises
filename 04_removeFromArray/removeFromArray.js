17e06621be55f34f983e066029f117a30f87e7db
Chrisvb99 <chrisvon3@gmail.com>
2022-09-15 14:06:33 +0200

    Complete exercises 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+  let outputArray = array;
+  for(let i=array.length;i>=0;i--){
+    for(let j=1;j<arguments.length;j++){
+      if(array[i] === arguments[j]){
+        array.splice(i,1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+  let sum = 0;
+  if(startNum > endNum) [startNum, endNum] = [endNum, startNum]
+  if(startNum < 0 || endNum < 0) return "ERROR"
+  if(typeof startNum !== 'number' || typeof endNum !== 'number') return "ERROR"
 
+  for(let i=startNum;i<=endNum;i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
